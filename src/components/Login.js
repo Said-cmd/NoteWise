c12f6307e714ab4ec24191e65430bd5496537868
Said Hussein <saidhussein12342@gmail.com>
2022-07-28 13:31:49 +0300

    Added more required components.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,22 @@
 import React from "react";
 function Login() {
     return (
-    <main>
-    </main>
+    <div className="login-holder">
+      <form class="ui form">
+        <div class="field">
+        <label>First Name</label>
+        <input 
+         placeholder="First Name"
+         />
+        </div>
+        <div class="field">
+        <label>Last Name</label>
+        <input 
+        placeholder="Last Name"/>
+        </div>
+        <button class="ui button" type="submit">Submit</button>
+        </form>
+        </div>
     );
 }
 export default Login;
