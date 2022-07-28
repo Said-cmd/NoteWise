ef0380c16e69acf62dd54c98871cec10955e0ccd
Said Hussein <saidhussein12342@gmail.com>
2022-07-28 14:10:47 +0300

    Login form styled and completed using bootstrap.

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,23 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 function Login() {
     return (
     <div className="login-holder">
-      <form class="ui form">
-        <div class="field">
-        <label>First Name</label>
-        <input 
-         placeholder="First Name"
-         />
-        </div>
-        <div class="field">
-        <label>Last Name</label>
-        <input 
-        placeholder="Last Name"/>
-        </div>
-        <button class="ui button" type="submit">Submit</button>
-        </form>
+      <Form>
+        <h1 className="login-title">Welcome to NoteWise</h1>
+        <h2 className="login-subtitle">Please login to continue</h2>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control type="email" placeholder="Username" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
         </div>
     );
 }
