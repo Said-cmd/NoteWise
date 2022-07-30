afe80d44db807b0591c1565a5fb7cc285043efba
Said Hussein <saidhussein12342@gmail.com>
2022-07-30 22:50:27 +0300

    Updating the search.js component.

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,12 +4,12 @@ import { MdSearch } from "react-icons/md"
 function Search() {
     return (
         <div className="search">
-      <input
-        type="text"
-        placeholder="Search your Recent Notes"
-      />
-      <MdSearch className='search-icons' size='1.3em' />
-    </div>
+             <MdSearch className="search-icon" size="1.5em" />
+             <input 
+             type="text" 
+             placeholder="Search your notes..."
+             />
+        </div>
     );
 }
 
