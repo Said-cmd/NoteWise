de4de02c6fc05fbd8e51adaab895fb5617857d1a
Said Hussein <saidhussein12342@gmail.com>
2022-07-30 22:54:23 +0300

    Updating the App.js component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import Landing from './Landing';
-import Navbar from "./Navbar"
-import Search from './Search';
+import React, { useEffect, useState } from "react";
+import NotesList from "./components/NotesList";
+import Search from "./components/Search";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/notes")
+      .then((r) => r.json())
+      .then((notes) => setNotes(notes))
+  },[])
+
+  function addNote(content) {
+    const date = new Date()
+    const newNote = {
+      content: content,
+      date: date.toLocaleDateString()
+    }
+    fetch("http://localhost:5000/notes", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newNote)
+    })
+    .then((r) => r.json())
+    .then((newNote) => {
+      const newNotes = [...notes, newNote]
+      setNotes(newNotes)
+  })
+
+}
   return (
-	<div className='App'>
-		{/*<Navbar />*/}
-		<Search />
-		{/*<Landing />*/}
-	</div>
+      <div className="container">
+        <Search />
+        <NotesList notes={notes} setNotes={setNotes} handleAddNote={addNote} />
+      </div>
   );
 }
 
