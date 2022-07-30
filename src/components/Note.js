14861deeaf93cd1d39b72787dd92ff5dccc35f37
Said Hussein <saidhussein12342@gmail.com>
2022-07-30 22:48:24 +0300

    Updating the Note.js component.

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { MdDeleteForever } from "react-icons/md"
 
-function Note() {
+function Note({content, date, notes, setNotes, id }) {
+
+    function handleDeleteNote() {
+        fetch(`http://localhost:5000/notes/${id}`, {
+            method: "DELETE"
+        })
+            .then((r) => r.json())
+            .then(() => {
+                const currentNotes = notes.filter((note) => {
+                    return note.id !== id
+                })
+                setNotes(currentNotes)
+            })
+    }
+
     return (
-
+        <div className="note">
+        <span>{ content }</span>
+        <div className="note-footer">
+            <small>{ date }</small>
+            <MdDeleteForever onClick={handleDeleteNote} className="delete-icon" size="1.5em" />
+        </div>
+        </div>
     );
 }
 
