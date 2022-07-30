3d4c9f13623c8b2f3cb5084290595e2e9423ad8e
Said Hussein <saidhussein12342@gmail.com>
2022-07-30 22:49:32 +0300

    Updating the NoteList.js component.

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Note from "./Note";
+import AddNote from "./AddNote";
 
-function NoteList() {
+function NoteList({ notes, setNotes, handleAddNote }) {
+    const notesList = notes.map((note) => {
+        return <Note
+        notes={notes}
+        setNotes={setNotes}
+        id={note.id}
+        key={note.id}
+        content={note.content}
+        date={note.date}
+        />
+    })
     return (
-
+        <div className="notes-list">
+            {notesList}
+            <AddNote handleAddNote={handleAddNote}/>
+        </div>
     );
 }
 
